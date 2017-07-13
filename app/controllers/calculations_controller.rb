be5e363c332aad106f86b2c38a9009527ac0c1db
Pooja Parthasarathy <ppartha0@chicagobooth.edu>
2017-07-13 01:59:52 +0000

    Added new pages

---
+++ app/controllers/calculations_controller.rb
@@ -0,0 +1,45 @@
+class CalculationsController < ApplicationController
+    
+    def flex_square
+        # The incoming parameters for this action look like {"a_number" => "5"}
+        # Rails stores that has in a variable called params
+        
+        @user_input = params["a_number"].to_i
+        @squared_number = @user_input**2
+        
+        render("calculations/flexible_square_template.html.erb")
+    end
+    
+    def flex_square_root
+        @user_input = params["a_number"].to_i
+        @square_root = @user_input**0.5
+        
+    render("calculations/flexible_square_root_template.html.erb")
+    end
+    
+    def flex_payment
+        @basis_points = params["basis_points"].to_i
+        @number_of_years = params["number_of_years"].to_i
+        @present_value = params["present_value"].to_i
+        monthly_rate = @basis_points/1200.0
+        num_months = @number_of_years*12
+        ### Calc: P = r(PV) / (1-(1+r)^-n)
+        
+        @monthly_payment = (monthly_rate*@present_value)/ (1 - ((1+monthly_rate)**-num_months))
+        #@monthly_payment = monthly_rate
+        
+    render("calculations/flexible_payment_template.html.erb")
+    end
+    
+    def square_form
+    
+    render("calculations/square_form_template.html.erb")
+    end
+    
+    def process_square
+        @user_input = params["the_user_number"].to_i
+        @processed_square = @user_input**2
+        render("calculations/process_square_template.html.erb")
+    end
+    
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  
+  get("/flexible/square/:a_number", { :controller =>"calculations", :action => "flex_square"})
+  get("/flexible/square_root/:a_number", { :controller =>"calculations", :action => "flex_square_root"})
+  get("/flexible/payment/:basis_points/:number_of_years/:present_value", { :controller =>"calculations", :action => "flex_payment"})
+  
+  get("/square/new", { :controller => "calculations", :action => "square_form"})
+  get("/square/results", { :controller => "calculations", :action => "process_square"})
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   mount WebGit::Engine, at: "/rails/git"
 end
